3b723f38362bb440104891461197b1466745045f
Russell Wallace <russell.wallace@gmail.com>
2024-04-26 14:41:00 +0100

    refactor

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ namespace TestAzureAIOpenAI {
 internal class Program {
 	static void Main(string[] args) {
 		var apiKey = Environment.GetEnvironmentVariable("OPENAI_API_KEY") ?? throw new Exception("OPENAI_API_KEY not set");
-		var client = new OpenAIClient(apiKey);
+		OpenAIClient client = new(apiKey);
 	}
 }
 }
