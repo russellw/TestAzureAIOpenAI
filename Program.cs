11797be32ec657a4038199aa5d3bea7407bd4944
Russell Wallace <russell.wallace@gmail.com>
2024-04-26 14:37:15 +0100

    refactor

--- Program.cs
+++ Program.cs
@@ -3,9 +3,7 @@ using Azure.AI.OpenAI;
 namespace TestAzureAIOpenAI {
 internal class Program {
 	static void Main(string[] args) {
-		var apiKey = Environment.GetEnvironmentVariable("OPENAI_API_KEY");
-		if (apiKey == null)
-			throw new Exception("OPENAI_API_KEY not set");
+		var apiKey = Environment.GetEnvironmentVariable("OPENAI_API_KEY") ?? throw new Exception("OPENAI_API_KEY not set");
 		var client = new OpenAIClient(apiKey);
 	}
 }
